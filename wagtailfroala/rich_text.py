d64d00831375489cfacefa7af697a9e76fb7f175
flytrap <hiddenstat@gmail.com>
2018-11-08 15:03:40 +0800

    new: add wagtail 2 support

--- wagtailfroala/rich_text.py
+++ wagtailfroala/rich_text.py
@@ -1,14 +1,25 @@
 from __future__ import absolute_import, unicode_literals
 
+import functools
 import json
-
 from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.forms import Media, widgets
-
+from wagtail import __version__ as WAGTAIL_VERSION
 from wagtail.utils.widgets import WidgetWithScript
-from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
-from wagtail.wagtailcore.rich_text import DbWhitelister, expand_db_html
+
+if WAGTAIL_VERSION >= '2.0':
+    from wagtail.admin.edit_handlers import RichTextFieldPanel
+    from wagtail.admin.rich_text.converters.editor_html import DbWhitelister
+    from wagtail.core.rich_text import expand_db_html
+
+    db_white_lister = DbWhitelister(DbWhitelister.element_rules)
+else:
+    from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
+    from wagtail.wagtailcore.rich_text import DbWhitelister, expand_db_html
+
+    expand_db_html = functools.partial(expand_db_html, for_editor=True)
+    db_white_lister = DbWhitelister
 
 
 class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
@@ -26,12 +37,12 @@ class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
     def get_panel(self):
         return RichTextFieldPanel
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, **kwargs):
         if value is None:
             translated_value = None
         else:
-            translated_value = expand_db_html(value, for_editor=True)
-        return super(FroalaRichTextArea, self).render(name, translated_value, attrs)
+            translated_value = expand_db_html(value)
+        return super(FroalaRichTextArea, self).render(name, translated_value, attrs, **kwargs)
 
     def render_js_init(self, id_, name, value):
         return "makeFroalaRichTextEditable({0}, {1});".format(
@@ -43,7 +54,7 @@ class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
         original_value = super(FroalaRichTextArea, self).value_from_datadict(data, files, name)
         if original_value is None:
             return None
-        return DbWhitelister.clean(original_value)
+        return db_white_lister.clean(original_value)
 
     @property
     def media(self):
